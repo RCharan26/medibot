31d68565f07573e68b6bcfa7f7e2d16e7a6f9a44
RCharan26 <159528085+RCharan26@users.noreply.github.com>
2025-09-15 19:19:35 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Genarative AI Project",
+    version="0.0.0",    
+    author='Ramcharan',
+    author_email='pisatiramcharan33@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
